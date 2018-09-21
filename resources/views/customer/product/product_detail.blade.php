f5a4c016ef6019f2c0d668d2a73d084aa9a3a43a
BichDanh1004 <bichdanh1004@gmail.com>
2018-09-21 11:20:52 +0700

    shoe_store

--- resources/views/customer/product/product_detail.blade.php
+++ resources/views/customer/product/product_detail.blade.php
@@ -1,31 +1,46 @@
 @extends('customer.layout')
 @section('custom-container')
 
-<div class="inner-header">
-		<div class="container">
-			<div class="pull-left">
-				<h4>Product</h4>
-			</div>
-			<div class="pull-right">
-				<div class="beta-breadcrumb font-large">
-					<a href="{{url('adidas_shoe')}}">Home</a> / <span>Product</span>
-				</div>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-	</div>
+<h3 class="mb-5 mx-5">PRODUCT DETAIL</h3>
 
 <div class="container">
 			<div class=" d-flex flex-row">
-                                <div class="col-4 ">
+                   
+                <div class="col-8">
+                        <div class="row">
+                            <div class="col-6">
+                                    <div class="card">
+                                            <div class="img-thumbnail image-hover">
+                                            <img class="card-img-top" src="{{asset('./images/images12.jpg')}}" >
+                                            </div>
+                                        </div> 
+                            </div>
+                            <div class="col-6">
+                                    <div class="col-6 ">
+                                            <div class="card">
+                                                <div class="img-thumbnail image-hover">
+                                                <img class="card-img-top" src="{{asset('./images/images12.jpg')}}" >
+                                                </div>
+                                            </div>  
+              
+                                    </div>
+            
+                                    <div class="col-6 ">
                                         <div class="card">
                                             <div class="img-thumbnail image-hover">
                                             <img class="card-img-top" src="{{asset('./images/images12.jpg')}}" >
                                             </div>
-                                            </div>  
-          
-						</div>
-						<div class="col-8 mt-5">
+                                        </div>  
+                                    </div>
+                            </div>
+                        </div>
+                                   
+                            </div>
+                   
+    
+
+            
+						<div class="col-4 mt-5">
 							<div class="single-item-body">
 								<p class="single-item-title">Sample Woman Top</p>
 								<p class="single-item-price">
@@ -60,12 +75,10 @@
                             </div>
                            
                         </div>
-                        
-            </div>
-
+                </div>
         </div> 
         <div class="container">
-                    <h2 class="mt-3 mb-3 d-flex justify-content-center ">Chi tiết sản phẩm</h2>
+                    <h2 class="mt-5 mb-3 d-flex justify-content-center ">Chi tiết sản phẩm</h2>
                     <p >Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
                     <p>Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequaturuis autem vel eum iure reprehenderit qui in ea voluptate velit es quam nihil molestiae consequr, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
          
